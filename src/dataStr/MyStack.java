7ac162dd748f22cc8b6e459e900b303e99968ea5
Sophi0 <s21hohlsofj@venta.lv>
2023-03-13 19:33:15 +0200

    Add: push() and pop() functions

--- src/dataStr/MyStack.java
+++ src/dataStr/MyStack.java
@@ -47,4 +47,81 @@ public class MyStack {
 	public int howManyElements() {
 		return elementCounter;
 	}
+	
+	/*
+	 * push() funkcija
+	 */
+	
+	public void push(char item) {					//funkcija ņem char item kā parametru,
+		if(elementCounter == stackSize - 1) {		//ja steka augšdaļā ir max izmēra, tad funkcija izmest paziņojumu, 
+			System.out.println("Stack Overflow");	//kas norāda, ka steks ir pilns
+		}
+		else {
+			elementCounter++;
+			elements[elementCounter] = item;
+		}
+	}
+	
+	//varbūt padomāt kā citādāk izvadīt push() funkciju
+	/*
+	public void push(char item) {
+		if(isFull()) {
+			
+		}
+	}
+	*/
+	
+	/*
+	 * pop() funkcija
+	 */
+	/*
+	 * //1.create a definition of function remove
+		public void remove(int index) throws Exception{
+	//2.verify isEmpty
+			if(isEmpty()) {
+				throw (new Exception("Array is empty ad it is not possible to remove elements"));
+				}
+			else {
+	//3.verify the index - is it appropriate
+			if(index < 0 || index >= elementCounter) {
+				throw (new Exception("Wrong index"));
+			}
+			else {
+	//4.copy elements from index to end
+				for(int i = index; i < elementCounter - 1; i++) {
+					elements[i] = elements[i + 1];	//pa labi
+				}
+	//5.initialize the last element with NULL symbol (int value is 0)			
+				elements[elementCounter - 1] = 0; //NUL symbol
+	//6.decrease element
+				elementCounter--;
+	//for optimization
+	//elements[--elementCounter] = 0;			
+			}
+		}
+	}
+	 */
+	//1.create a definition of function pop()
+	public void pop(int item) throws Exception {
+	//2.verify isEmpty
+		if(isEmpty()) {
+			throw(new Exception("Stack is empty and it is not possible to remove elements"));	
+		}
+		else {
+	//3.verify the index - is it appropriate
+			if(item < 0 || item >= elementCounter) {
+				throw(new Exception("Wrong index"));
+			}
+			else {
+	//4.copy elements from index to end
+				for(int i = item; i < elementCounter - 1; i++) {
+					elements[i] = elements[i + 1]; 	//iet pa labi
+				}
+	//5.initialize the last element with NULL symbol (int value is 0)
+				elements[elementCounter - 1] = 0;
+	//6.decrease element
+				elementCounter--;
+			}
+		}
+	}
 }
